852ce924befd57c0980bce44ffd1c3f88a9cfbd3
BaiYan321 <35852954+BaiYan321@users.noreply.github.com>
2024-05-27 00:49:56 +0000

    Add dockerfile

---
+++ week1_basics/2_docker_sql/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[0]
+# some fancy stuff with pandas
+
+print(f'job finished succefully for day = {day}') #used to check if pandas can be imported successfully
